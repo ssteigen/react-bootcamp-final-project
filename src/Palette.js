1bda23f59872f03de0e2075d16d5731a69910451
Steigen, Savanah <steigen@theinstitutes.org>
2022-06-11 10:26:23 -0400

    Create basic color box grid

--- src/Palette.js
+++ src/Palette.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { ColorBox } from "./ColorBox";
+
+import './Palette.css';
 
 export class Palette extends Component {
   render() {
+    const colorBoxes = this.props.colors.map((color) => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
+
     return (
       <div className="Palette">
         {/* Navbar goes here */}
-        <div className="Palette-colors">
-          {/* bunch of color boxes */}
-        </div>
+        <div className="Palette-colors">{colorBoxes}</div>
         {/* footer goes here */}
       </div>
-    )
+    );
   }
 }

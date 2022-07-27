a334373737cc6a3e9a91672c22814085d3d89506
Steigen, Savanah <steigen@theinstitutes.org>
2022-07-27 11:17:47 -0400

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Palette } from './Palette';
 import { seedColors } from "./seedColors";
 
@@ -6,9 +7,16 @@ import {generatePalette} from "./colorHelpers";
 
 function App() {
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" render={() => <h1>Palette List Goes here</h1>}/>
+          <Route exact path="/palette/:id" render={() => <h1>Individual palette goes here</h1>} />
+        </Switch>
+
+        {/* <Palette palette={generatePalette(seedColors[4])} /> */}
+      </div>
+    </BrowserRouter>
   );
 }
 

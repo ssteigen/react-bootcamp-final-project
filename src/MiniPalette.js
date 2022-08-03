17662164917cdb17946546f3882989223931e737
Steigen, Savanah <steigen@theinstitutes.org>
2022-08-03 10:44:07 -0400

    Start styling palettelist

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./MiniPalette.css";
 
 export default function MiniPalette(props) {
-  const { paletteName, emoji } = props;
+  const { paletteName, colors, emoji } = props;
 
   return (
     <div className="MiniPalette">
-      <div className="MiniPalette-colors"></div>
+      <div className="MiniPalette-colors">
+
+      </div>
       <h5 className="MiniPalette-title">
         {paletteName} <span className="MiniPalette-emoji">{emoji}</span>
       </h5>
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import "./PaletteList.css";
 import MiniPalette from "./MiniPalette";
 
 export class PaletteList extends Component {
@@ -9,14 +9,20 @@ export class PaletteList extends Component {
 
     return (
       <div className="PaletteList">
-        <h1>React Colors</h1>
-        {palettes.map((palette) => (
-          <div key={palette.id}>
-            <Link to={`/palette/${palette.id}`}>
-              <MiniPalette {...palette} />
-            </Link>
+        <div className="PaletteList-container">
+          <nav className="PaletteList-nav">
+            <h1>React Colors</h1>
+          </nav>
+          <div className="PaletteList-palettes">
+            {palettes.map((palette) => (
+              <div key={palette.id}>
+                <Link to={`/palette/${palette.id}`}>
+                  <MiniPalette {...palette} />
+                </Link>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     );
   }
